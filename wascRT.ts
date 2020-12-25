8b905f11e17ef8aa87d5cd96e85169bab5f24e64
hexxone <dominics.pc@gmail.com>
2020-12-26 00:55:07 +0100

    add non-worker, Split RT, use assemblyscriptloader

---
+++ wascRT.ts
@@ -0,0 +1,73 @@
+
+
+export default function MakeRT(memory: WebAssembly.Memory, allocF64, allocU32) {
+    
+    var mem, F64, U32;
+    var cached = new WeakMap();
+    
+    function refreshMemory() {
+        if (mem !== memory.buffer) {
+            mem = memory.buffer;
+            U32 = new Uint32Array(mem);
+            F64 = new Float64Array(mem);
+        }
+    }
+    
+    function newF64Array(typedArray) {
+        var ptr;
+        if (!cached.has(typedArray)) {
+            ptr = allocF64(typedArray.length);
+            refreshMemory();
+            cached.set(typedArray, ptr);
+        } else {
+            refreshMemory();
+            ptr = cached.get(typedArray);
+        }
+        const dataStart = (U32[ptr >>> 2] >>> 2) + 2;
+        F64.set(typedArray, dataStart >>> 1);
+        return ptr;
+    }
+    
+    function getF64Array(ptr) {
+        refreshMemory();
+        ptr >>>= 2;
+    
+        const offset = (U32[ptr] >>> 2) + 2;
+        const len = U32[ptr + 1];
+    
+        return F64.subarray(offset, offset + len);
+    }
+    
+    function newU32Array(typedArray) {
+        var ptr;
+        if (!cached.has(typedArray)) {
+            ptr = allocU32(typedArray.length);
+            refreshMemory();
+            cached.set(typedArray, ptr);
+        } else {
+            refreshMemory();
+            ptr = cached.get(typedArray);
+        }
+        const dataStart = (U32[ptr >>> 2] >>> 2) + 2;
+        U32.set(typedArray, dataStart);
+        return ptr;
+    }
+    
+    function getU32Array(ptr) {
+        refreshMemory();
+        ptr >>>= 2;
+    
+        const offset = (U32[ptr] >>> 2) + 2;
+        const len = U32[ptr + 1];
+    
+        return U32.subarray(offset, offset + len);
+    }
+    
+
+    return {
+        newF64Array,
+        getF64Array,
+        newU32Array,
+        getU32Array,
+    };
+}
