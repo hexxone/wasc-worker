f11ac54e3937ddc11549bbb5d72bed0b9ffb82cf
hexx.one <5312542+hexxone@users.noreply.github.com>
2021-06-02 13:21:01 +0200

    Lots of refactoring & updated docs

--- WascInterface.ts
+++ WascInterface.ts
@@ -7,44 +7,136 @@
 * See LICENSE file in the project root for full license information.
 */
 
-import {ResultObject, ASUtil} from '@assemblyscript/loader';
 
 /**
-* basic interface for loading a module
-* @implements {ResultObject}
-* @see https://www.assemblyscript.org/loader.html
+ * Utility mixed in by the loader.
+ * @public
+ */
+export interface ASUtil {
+	memory?: WebAssembly.Memory;
+	table?: WebAssembly.Table;
+
+	/** Explicit start function, if requested. */
+	_start(): void;
+
+	/** Copies a string's value from the module's memory. */
+	__getString(ptr: number): string;
+	/** Copies an ArrayBuffer's value from the module's memory. */
+	__getArrayBuffer(ptr: number): ArrayBuffer;
+
+	/** Copies an array's values from the module's memory. Infers the array type from RTTI. */
+	__getArray(ptr: number): number[];
+	/** Copies an Int8Array's values from the module's memory. */
+	__getInt8Array(ptr: number): Int8Array;
+	/** Copies an Uint8Array's values from the module's memory. */
+	__getUint8Array(ptr: number): Uint8Array;
+	/** Copies an Uint8ClampedArray's values from the module's memory. */
+	__getUint8ClampedArray(ptr: number): Uint8ClampedArray;
+	/** Copies an Int16Array's values from the module's memory. */
+	__getInt16Array(ptr: number): Int16Array;
+	/** Copies an Uint16Array's values from the module's memory. */
+	__getUint16Array(ptr: number): Uint16Array;
+	/** Copies an Int32Array's values from the module's memory. */
+	__getInt32Array(ptr: number): Int32Array;
+	/** Copies an Uint32Array's values from the module's memory. */
+	__getUint32Array(ptr: number): Uint32Array;
+	/** Copies an Int32Array's values from the module's memory. */
+	__getInt64Array?(ptr: number): BigInt64Array;
+	/** Copies an Uint32Array's values from the module's memory. */
+	__getUint64Array?(ptr: number): BigUint64Array;
+	/** Copies a Float32Array's values from the module's memory. */
+	__getFloat32Array(ptr: number): Float32Array;
+	/** Copies a Float64Array's values from the module's memory. */
+	__getFloat64Array(ptr: number): Float64Array;
+
+	/** Gets a live view on an array's values in the module's memory. Infers the array type from RTTI. */
+	__getArrayView(ptr: number): Uint32Array | Float32Array | Float64Array | Uint8Array | Int8Array | Uint16Array | Int16Array | Int32Array | BigUint64Array | BigInt64Array;
+	/** Gets a live view on an Int8Array's values in the module's memory. */
+	__getInt8ArrayView(ptr: number): Int8Array;
+	/** Gets a live view on an Uint8Array's values in the module's memory. */
+	__getUint8ArrayView(ptr: number): Uint8Array;
+	/** Gets a live view on an Uint8ClampedArray's values in the module's memory. */
+	__getUint8ClampedArrayView(ptr: number): Uint8ClampedArray;
+	/** Gets a live view on an Int16Array's values in the module's memory. */
+	__getInt16ArrayView(ptr: number): Int16Array;
+	/** Gets a live view on an Uint16Array's values in the module's memory. */
+	__getUint16ArrayView(ptr: number): Uint16Array;
+	/** Gets a live view on an Int32Array's values in the module's memory. */
+	__getInt32ArrayView(ptr: number): Int32Array;
+	/** Gets a live view on an Uint32Array's values in the module's memory. */
+	__getUint32ArrayView(ptr: number): Uint32Array;
+	/** Gets a live view on an Int32Array's values in the module's memory. */
+	__getInt64ArrayView?(ptr: number): BigInt64Array;
+	/** Gets a live view on an Uint32Array's values in the module's memory. */
+	__getUint64ArrayView?(ptr: number): BigUint64Array;
+	/** Gets a live view on a Float32Array's values in the module's memory. */
+	__getFloat32ArrayView(ptr: number): Float32Array;
+	/** Gets a live view on a Float64Array's values in the module's memory. */
+	__getFloat64ArrayView(ptr: number): Float64Array;
+
+	/** Allocates an instance of the class represented by the specified id. */
+	__new(size: number, id: number): number;
+	/** Allocates a new string in the module's memory and returns a reference (pointer) to it. */
+	__newString(str: string): number;
+	/** Allocates a new array in the module's memory and returns a reference (pointer) to it. */
+	__newArray(id: number, values: ArrayLike<number>): number;
+	/** Retains a reference to a managed object externally, making sure that it doesn't become collected prematurely. Returns the pointer. */
+	__retain(ptr: number): number;
+	/** Releases a previously retained reference to a managed object, allowing the runtime to collect it once its reference count reaches zero. */
+	__release(ptr: number): void;
+	/** Forcefully resets the heap to its initial offset, effectively clearing dynamic memory. Stub runtime only. */
+	__reset?(): void;
+	/** Tests whether a managed object is an instance of the class represented by the specified base id. */
+	__instanceof(ptr: number, baseId: number): boolean;
+	/** Forces a cycle collection. Only relevant if objects potentially forming reference cycles are used. */
+	__collect(): void;
+  }
+
+
+/**
+* parameters for the inline run-function
+* @see {WascInterface}
+* @public
 */
-export class WascBasic implements ResultObject {
+export interface WascBasic {
 	/**
 	* loaded WebAssembly.Module
 	* @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WebAssembly/Module
+	* @public
 	*/
 	module: WebAssembly.Module;
+
 	/**
 	* loaded WebAssembly.Instance
 	* @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WebAssembly/Instance
+	* @public
 	*/
 	instance: WebAssembly.Instance;
+
 	/**
 	* injected assemblyscript utility functions
-	* @see https://www.assemblyscript.org/loader.html#module-instance-utility
+	* @see {ASUtil}
+	* @public
 	*/
 	exports: ASUtil;
 }
 
 /**
 * shared interface for loading a module
-* @extends {WascBasic}
+* @see {WascLoader}
+* @public
 */
-export class WascInterface extends WascBasic {
+export interface WascInterface {
+	/**
+	* injected assemblyscript utility functions
+	* @see {ASUtil}
+	* @public
+	*/
+	exports: ASUtil;
+
 	/**
 	* Worker-context run function
+	* @public
 	*/
-	run: (func: typeof WascRun, ...params) => Promise<any>;
+	run: (func: (params: WascBasic & {params:any}) => void, ...params) => Promise<any>;
 }
-
-/**
-* The call-struct for the internal run function
-* @param {WascBasic} params the data passed into the run-function
-*/
-export function WascRun(params: WascBasic & {params:any}) {}
---
+++ index.ts
@@ -0,0 +1,198 @@
+/**
+* @author Matteo Basso @https://github.com/mbasso
+* @author hexxone / https://hexx.one
+*
+* @license
+* Copyright (c) 2021 hexxone All rights reserved.
+* Licensed under the GNU GENERAL PUBLIC LICENSE.
+* See LICENSE file in the project root for full license information.
+*
+*/
+
+import {WascLoader} from './WascLoader';
+import WascWorker from 'worker-loader!./Wasc';
+
+import {Smallog} from '../Smallog';
+
+import {WascInterface} from './WascInterface';
+import {ACTIONS, getTransferableParams, myFetch} from './WascUtil';
+
+/**
+* Initializes a new WebAssembly instance.
+* @param {string} source compiled .wasm module path
+* @param {number} initialMem initial webassembly memory (default 4096)
+* @param {Object} options passed to the module init
+* @param {boolean} useWorker use worker or inline
+* @return {Promise<WascInterface>} the initialized context
+* @public
+*/
+export function wascWorker(source: string, initialMem = 4096, options: any = {}, useWorker: boolean = true): Promise<WascInterface> {
+	return new Promise((resolve) => {
+		// decide whether to use worker-threading or inline embedding.
+		const loadWrk =(useWorker && typeof(Worker) !== 'undefined');
+		Smallog.debug(`Loading ${source} as ${loadWrk ? 'worker' : 'inline'} with data=${JSON.stringify(options)}`, '[WASC] ');
+		// initialize the actual module
+		const doLoad = loadWrk ? loadWorker : loadInline;
+		doLoad(source, initialMem, options).then((loaded) => {
+			resolve(loaded);
+		}).catch((err) => {
+			// something went south?
+			Smallog.error('init error: ' + err, '[WASC] ');
+		});
+	});
+}
+
+
+/**
+* @ignore
+* Inline loads a compiled webassembly module.
+* Basically the normal WebAssembly usage,
+* just with api- and "run()"-compatibility
+* @param {string} path compiled module path
+* @param {number} initialMem initial memory size in kb
+* @param {Object} options import Objects
+* @return {Promise<WascInterface>} module
+* @public
+*/
+function loadInline(path: string, initialMem: number, options: any): Promise<WascInterface> {
+	return new Promise(async (resolve) => {
+	// let ascExports: any;
+		const memory = new WebAssembly.Memory({initial: initialMem});
+		const staticImports = {
+			env: {
+				memory,
+				logf(value) {
+					console.log('F64: ' + value);
+				},
+				logi(value) {
+					console.log('U32: ' + value);
+				},
+			},
+		};
+
+		// get import object
+		const {getImportObject} = options;
+		const myImports = Object.assign({}, staticImports);
+		if (getImportObject !== undefined) {
+			Object.assign(myImports, getImportObject());
+		}
+
+		const byteModule = await myFetch(path);
+		const inst = new WascLoader().instantiateSync(byteModule, myImports);
+
+		/**
+		* Run a function inside the worker.
+		* @warning This is potentially dangerous due to eval!
+		* @param {string} func stringified function to eval inside worker context
+		* @param {Object} params Data to pass in
+		* @return {Object} eval result
+		*/
+		const run = (func, ...params) => {
+			return new Promise((res) => {
+				const fun = new Function(`return ${func}`)();
+				res(fun({
+					module: inst.module,
+					instance: inst.instance,
+					exports: inst.exports,
+					params,
+				}));
+			});
+		};
+
+		// we done here
+		resolve({
+			exports: inst.exports,
+			run,
+		});
+	});
+}
+
+
+/**
+* @ignore
+* Creates a Worker, then loads a compiled webassembly module inside,
+* then creates a Promise-interface for all functions and additionally
+* wraps a "run" function inside the worker.
+* @param {string} source compiled module path
+* @param {Object} options import Objects
+* @return {Promise<WascInterface>} module
+* @public
+*/
+function loadWorker(source: string, options): Promise<WascInterface> {
+	return new Promise((...reslv) => {
+		// WRAP IN WORKER
+		let promCnt = 0;
+		const promises = {};
+
+		const worker = new WascWorker(options);
+
+		worker.onmessage = (e) => {
+			const {id, result, action, payload} = e.data;
+
+			if (action === ACTIONS.COMPILE_MODULE) {
+				// COMPILE MODULE & RESOLVE EXPORTS
+				if (result === 0) {
+					// SUCCESS
+					const {exports} = payload;
+
+					promises[id][0]({
+
+						// wrap the returned context/thread exports into promises
+						exports: exports.reduce((acc, exp) => ({
+							...acc,
+							[exp]: (...params) => new Promise((...rest) => {
+								promises[++promCnt] = rest;
+								worker.postMessage({
+									id: promCnt,
+									action: ACTIONS.CALL_FUNCTION_EXPORT,
+									payload: {
+										func: exp,
+										params,
+									},
+								}, getTransferableParams(params));
+							}),
+						}), {}),
+
+						// export context/thread run function
+						run: (func, ...params) => new Promise((...rest) => {
+							promises[++promCnt] = rest;
+							worker.postMessage({
+								id: promCnt,
+								action: ACTIONS.RUN_FUNCTION,
+								payload: {
+									func: func.toString(),
+									params,
+								},
+							}, getTransferableParams(params));
+						}),
+
+					});
+				} else if (result === 1) {
+					// ERROR
+					promises[id][1](payload);
+				}
+
+			// CALL FUNCTION
+			} else if (
+				action === ACTIONS.CALL_FUNCTION_EXPORT ||
+				action === ACTIONS.RUN_FUNCTION
+			) {
+				promises[id][result](payload);
+			}
+
+			promises[id] = null;
+		};
+
+		promises[++promCnt] = reslv;
+
+		worker.postMessage({
+			id: promCnt,
+			action: ACTIONS.COMPILE_MODULE,
+			payload: source,
+		});
+	});
+}
+
+
+export * from './WascInterface';
+export * from './WascUtil';
