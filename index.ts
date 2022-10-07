6ec3442d45a882c141498ecb41d75816f5c8775e
hexx.one <5312542+hexxone@users.noreply.github.com>
2022-10-07 19:53:21 +0200

    fix: license

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
+/**
+ * @author hexxone / https://hexx.one
+ *
+ * @license
+ * Copyright (c) 2022 hexxone All rights reserved.
+ * Licensed under the GNU GENERAL PUBLIC LICENSE.
+ * See LICENSE file in the project root for full license information.
+ *
+ */
+
 export * from "./Wasc";
 export * from "./WascInterface";
 export * from "./WascUtil";
